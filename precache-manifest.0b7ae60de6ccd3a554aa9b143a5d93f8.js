5d9c55182ba6e6847cc9698b1e70a6647d28609f
Lucero Dn <lucero_dn@hotmail.es>
2019-05-10 13:24:56 -0500

    Updates

---
+++ precache-manifest.0b7ae60de6ccd3a554aa9b143a5d93f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc02702f7a78d7494c213aff7e898841",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bf7b17003f9418ab3cf",
+    "url": "/static/css/main.dc0606ae.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "0bf7b17003f9418ab3cf",
+    "url": "/static/js/main.e3eab64d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
