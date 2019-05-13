a8dac10a9c7daa29dc40058e43928ff1a83785be
Lucero Dn <lucero_dn@hotmail.es>
2019-05-12 23:45:26 -0500

    Se agrego la pagina de inicio con los archivos home.js y app.js

---
+++ burger-queen-project/src/Position.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Position extends React.Component {
+
+    render() {
+      return (
+        <div>
+     <button>Caja 1</button>
+     <button>Caja 2</button>
+     <button>Cocina</button>
+     </div>
+      );
+    }
+  }
+
