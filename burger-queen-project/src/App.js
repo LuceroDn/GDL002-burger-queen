e0a88faac59d14a190f03333af4e77d15d6c38ba
Lucero Dn <lucero_dn@hotmail.es>
2019-05-28 05:37:45 -0500

    Se cambio el router a HashRouter en app.js

--- burger-queen-project/src/App.js
+++ burger-queen-project/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import "./App.css";
 import Home from "./Components/Home";
 import Position from "./Components/Position";
@@ -10,15 +10,15 @@ import CheckoutTwo from "./Components/CheckoutTwo";
  class App extends Component {
   render() {
     return (
-    <BrowserRouter>
-          <Switch>
+    <HashRouter>
+          <div>           
               <Route exact path='/' component={Home} />
-              <Route path='/Home' component={Home} />
-              <Route path='/Position' component={Position} />
-              <Route path='/CheckoutOne' component={CheckoutOne} />
-              <Route path='/CheckoutTwo' component={CheckoutTwo} />
-          </Switch>
-      </BrowserRouter>
+            <Route path='/Home' component={Home} />
+            <Route path='/Position' component={Position} />
+            <Route path='/CheckoutOne' component={CheckoutOne} />
+            <Route path='/CheckoutTwo' component={CheckoutTwo} />
+          </div>   
+      </HashRouter>
     );
   }
 }
